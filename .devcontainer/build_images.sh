ad3db2ea51d8a4f0c4e74e55c20290e06a3f0334
Art Tay <tay.art.42@gmail.com>
2025-04-10 17:35:59 -0500

    Converted docker image to singularity for HPC use.

--- .devcontainer/build_images.sh
+++ .devcontainer/build_images.sh
@@ -24,12 +24,16 @@ esac
 img="${name}_${version}_${platform}_$(date "+%Y_%m_%d_%H_%M_%S").sif"
 
 # Build container with Docker
-docker-compose build --no-cache --progress=plain\
- --platform linux/${platform} -t ${name}:${version} .
+docker-compose build --progress=plain
+#  --platform linux/${platform} -t ${name}:${version} .
 
 # Convert Docker image to Singularity image
+unset DOCKER_HOST
+#export DOCKER_HOST=unix:///var/run/docker.sock
+#export DOCKER_HOST=tcp://docker-desktop:2375
+echo $DOCKER_HOST
 docker run -v /var/run/docker.sock:/var/run/docker.sock\
- -v $PWD:/output --privileged -t --rm singularityware/docker2singularity\
+ -v "$PWD:/output" --privileged -t --rm singularityware/docker2singularity\
  -n ${img} ${name}:${version}
 mv ${img%.sif}.simg ${img}
 
